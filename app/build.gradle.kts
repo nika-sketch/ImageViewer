4b112e8f67cd0bf290fb0b8d9b1576bb4e3918c4
Nikoloz Latsabidze <nilatsabidze@bog.ge>
2025-08-29 09:15:57 +0400

    removed deprecated jvmTarget with toolchain in gradle.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
   alias(libs.plugins.google.devtools.ksp)
 }
 
+kotlin {
+  jvmToolchain(11)
+}
+
 android {
   namespace = "com.example.imageviewer"
   compileSdk = 36
@@ -31,9 +35,6 @@ android {
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
-  }
-  kotlinOptions {
-    jvmTarget = "11"
   }
   buildFeatures {
     compose = true
